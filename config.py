be77321f2ae6743a1191908669afd4c11a438439
marvis <marvispy247@gmail.com>
2021-08-24 21:20:43 -0400

    updated config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_KEY = 'youralpacaapikey'
-API_SECRET = 'youralpacaapisecret'
+API_KEY = ''
+API_SECRET = ''
 
-WEBHOOK_PASSPHRASE = 'yourpassphrase'
+WEBHOOK_PASSPHRASE = ''
 DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
