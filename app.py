343489d45f9a57a8dbf5c0b28b00e89292d8da18
marvis <marvispy247@gmail.com>
2021-08-26 16:54:23 -0400

    Code repurposed

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import config, json, requests
 
 app = Flask(__name__)
 
-api = tradeapi.REST(config.API_KEY, config.API_SECRET, base_url='https://paper-api.alpaca.markets')
+# api = tradeapi.REST(config.API_KEY, config.API_SECRET, base_url='https://paper-api.alpaca.markets')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def dashboard():
-    orders = api.list_orders()
-    
-    return render_template('dashboard.html', alpaca_orders=orders)
+    # orders = api.list_orders()
+    return render_template('dashboard.html')#, alpaca_orders=orders)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -21,22 +20,22 @@ def webhook():
             'code': 'error',
             'message': 'nice try buddy'
         }
-    
+    # print(webhook_message['passphrase'])
     price = webhook_message['strategy']['order_price']
     quantity = webhook_message['strategy']['order_contracts']
     symbol = webhook_message['ticker']
     side = webhook_message['strategy']['order_action']
     
-    order = api.submit_order(symbol, quantity, side, 'limit', 'gtc', limit_price=price)
+    # order = api.submit_order(symbol, quantity, side, 'limit', 'gtc', limit_price=price)
 
     # if a DISCORD URL is set in the config file, we will post to the discord webhook
-    if config.DISCORD_WEBHOOK_URL:
-        chat_message = {
-            "username": "strategyalert",
-            "avatar_url": "https://i.imgur.com/4M34hi2.png",
-            "content": f"tradingview strategy alert triggered: {quantity} {symbol} @ {price}"
-        }
+    # if config.DISCORD_WEBHOOK_URL:
+    #     chat_message = {
+    #         "username": "strategyalert",
+    #         "avatar_url": "https://i.imgur.com/4M34hi2.png",
+    #         "content": f"tradingview strategy alert triggered: {quantity} {symbol} @ {price}"
+    #     }
 
-        requests.post(config.DISCORD_URL, json=chat_message)
+    #     requests.post(config.DISCORD_URL, json=chat_message)
 
     return webhook_message
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_KEY = ''
 API_SECRET = ''
 
-WEBHOOK_PASSPHRASE = ''
+WEBHOOK_PASSPHRASE = 'codemon'
 DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
